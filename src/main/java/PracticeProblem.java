36b2cbbfa66d849377d66fba6502800e3a628a34
Marcotian0211 <350327743@tdsb.ca>
2026-05-04 14:48:56 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,36 @@
+import java.util.Random;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+	public static int randomNumber(int num,int num1){
+     Random random =new Random();
+     int num2;
+	 num2=random.nextInt(num,num1+1);   
+	return num2;
+	}
+	public static char randomChar(String word){
+    Random random=new Random();
+	int num;
+	num=random.nextInt(word.length());
+	char word2=word.charAt(num);
+    return word2;
+	}
 
+
+public static int randomEvenNumber(int a, int b) {
+    Random rand = new Random();
+    
+    int min = Math.min(a, b);
+    int max = Math.max(a, b);
+    
+    int num;
+    
+    do {
+        num = rand.nextInt(max - min + 1) + min;
+    } while (num % 2 != 0); 
+    
+    return num;
+}
 }
